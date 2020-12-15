74945cf3fd7913c77c3cb4e84fbc0505da86afbf
kuswandi.ti <kuswandi.ti@gmail.com>
2020-12-15 23:40:11 +0700

    Create API : 15/12/2020

--- config/app.php
+++ config/app.php
@@ -175,8 +175,6 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
-        Laravel\Passport\PassportServiceProvider::class,
-
     ],
 
     /*
